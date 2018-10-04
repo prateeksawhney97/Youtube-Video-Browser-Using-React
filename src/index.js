f4d82cd9ef48fc09261f090413325bed5e905655
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-04 11:58:33 +0530

    first element using React

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 // Create a new component that will produce some HTML.
-const App = function() {
+const App = () => {
   return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML and put it in the page.
 //in the DOM.
+ReactDOM.render(<App />, document.querySelector('.container'));
