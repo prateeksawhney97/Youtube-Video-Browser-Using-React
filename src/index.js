f0b5aacfc6407a8958cd8991cda475cc34a90ed3
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-04 14:10:06 +0530

    starting with youtube api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyA3_rjS2sPfougHZPQ0Kggt1gWAYI20JWE';
+
 // Create a new component that will produce some HTML.
 const App = () => {
   return <div>Hi!</div>;
