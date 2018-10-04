1a57bd628a39beac61b34d30dff1304ccab7abfc
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-04 18:35:37 +0530

    updating the state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,11 +4,16 @@ class SearchBar extends Component {
 
   constructor(props) {
       super(props);
-      this.state = { term: '' }; 
+      this.state = { term: '' };
 }
 
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />;
+        Value of the input: {this.state.term}
+      </div>
+  );
   }
 }
 
