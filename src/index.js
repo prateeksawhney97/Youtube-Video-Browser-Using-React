d85dc983590aecad0d0f4e828657a47f5f1bdc6a
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-05 01:17:43 +0530

    selection of different videos in now possible

--- src/index.js
+++ src/index.js
@@ -10,17 +10,23 @@ const API_KEY = '';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = { videos: [],
+      selectedVideo: null
+     };
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({ videos: videos,
+        selectedVideo: videos[0]
+       });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }
