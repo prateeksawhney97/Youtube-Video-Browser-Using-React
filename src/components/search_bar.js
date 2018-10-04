690f6bcdbb4ad5f1aa740fad9367c79e47d03da6
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-04 17:44:51 +0530

    upgrading searchbar from functional to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const searchBar = () => {
-  return <input />;
-};
+class SearchBar extends React.Component {
+  render() {
+    return <input />;
+  }
+}
 
-export default searchBar;
+export default SearchBar;
