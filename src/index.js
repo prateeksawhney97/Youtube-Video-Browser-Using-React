bf0e0c33d7221ff07124a15e40048e49b33196c1
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-05 00:57:26 +0530

    video player set up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = '';
 
@@ -18,6 +19,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
