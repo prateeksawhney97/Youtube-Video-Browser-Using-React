270ab1470d1a1ead1dbd06bf642194c486e5a2ef
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-04 18:04:32 +0530

    using arrow functions to clean up code of event handlers

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,11 +2,7 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <input onChange={this.onInputChange} />;
-  }
-
-  onInputChange(event) {
-    console.log(event.target.value);
+    return <input onChange={event => console.log(event.target.value)} />;
   }
 }
 
