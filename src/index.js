5aa25ac772245ffe0c725f1cfa4d51e7f987b1dc
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-04 20:50:33 +0530

    refracting functional to class

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 
 const API_KEY = '';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
-
-// Create a new component that will produce some HTML.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it in the page.
-//in the DOM.
 ReactDOM.render(<App />, document.querySelector('.container'));
