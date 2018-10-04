c833a8b6cc898a352d3525df73b06d908c9873f8
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-04 11:26:54 +0530

    JSX

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 // Create a new component that will produce some HTML.
-
+const App = function() {
+  return <div>Hi!</div>;
+}
 
 // Take this component's generated HTML and put it in the page.
+//in the DOM.
