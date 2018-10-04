1fcb5af5c20fa004e0be98b182162a3fbaed4758
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-04 20:35:52 +0530

    fetching video results

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-//const API_KEY = '';
+const API_KEY = '';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+});
 
 // Create a new component that will produce some HTML.
 const App = () => {
