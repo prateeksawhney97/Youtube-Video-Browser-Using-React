fc9dbb379fa3f11239b19de7fe96538768b18ffe
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-05 08:36:49 +0530

    user search defined

--- src/index.js
+++ src/index.js
@@ -13,16 +13,21 @@ class App extends Component {
     this.state = { videos: [],
       selectedVideo: null
      };
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+     this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({ videos: videos,
         selectedVideo: videos[0]
        });
     });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
         onVideoSelect={selectedVideo => this.setState({selectedVideo})}
